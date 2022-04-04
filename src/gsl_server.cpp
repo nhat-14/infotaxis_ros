0592481c569a8b162ec10a7f24e79bee832ab925
Luong Duc Nhat <nhat.luongduc@gmail.com>
2022-04-04 15:52:24 +0900

    delete the uses of action and class to simply start the search

--- src/gsl_server.cpp
+++ src/gsl_server.cpp
@@ -1,11 +1,11 @@
-#include "gsl_server.h"
 #include "gsl_infotaxis.h"
 
-//================================== CPT ALGORITHMS ===================================
-int CGSLServer::doInfotaxis() {
+int main(int argc, char** argv) {
+    ros::init(argc,argv,"gsl_ac");
     ros::NodeHandle nh("~");
     InfotaxisGSL infotatic(&nh);
     ros::Rate loop_rate(10);
+    
     while(ros::ok() && infotatic.checkSourceFound() == -1) {
         ros::spinOnce();
         if(!infotatic.get_inMotion()) {
@@ -25,36 +25,20 @@ int CGSLServer::doInfotaxis() {
         }
         loop_rate.sleep();
     }
-    return infotatic.checkSourceFound();
-}
 
-//===================== Action Server Callback when Goal is received ====================
-void CGSLServer::executeCB(const gsl_actionserver::gsl_action_msgGoalConstPtr &goal) {
-    int is_found = doInfotaxis(); // res: 0(fail) 2(cancelled) 1(sucess)
+
+    int is_found = infotatic.checkSourceFound(); // res: 0(fail) 2(cancelled) 1(sucess)
 
     if (is_found == 1) { 
-        result_.success = 1;
         ROS_INFO("[GSL_server]: Found the emission source!! ");
     }
     else if (is_found == 2) { // Canceled/Preempted by user?
-        result_.success = 0;
         ROS_INFO("[GSL_server]: Action cancelled/preemted");
     }
     else {
-        result_.success = 0;
         ROS_INFO("[GSL_server]: Couldn't find the gas source! SORRY!");
     }
-    as_.setSucceeded(result_);
     ros::shutdown();
-}
-
-
-//================================== MAIN ===========================================
-
-int main(int argc, char** argv) {
-    ros::init(argc,argv,"gsl_ac");
-    CGSLServer gsl_node("gsl");
-    ROS_INFO("[GSL_server] GSL action server is ready for action!...");
     ros::spin();
     return 0;
 }
